f6a3fca744a039d2698c6c77e2049eb6677927af
Vamshi Ponnapalli <vponnap2@VPONNAP2-M03.hsd1.ma.comcast.net>
2021-12-10 19:29:56 -0500

    Implement two way communication

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,20 @@
 #endif
 #include <espnow.h>
 
-#define __NODE_TYPE 1
 #define LOG_ENABLED
 #define LOG_LEVEL LOG_LEVEL_DEBUG
 #include "log.h"
+
+// #define __CONTROLLER_MODE
 
 u8 PEER_ADDRESSES[][6] = {
   {0x18, 0xFE, 0x34, 0xD3, 0xF1, 0x21}, // White
   {0x18, 0xFE, 0x34, 0xD4, 0x82, 0x2A} // Blue
 };
+u8 PEER_ADDRESS_LEN = 2;
 
 u64 last_time = 0;
-u64 send_interval = 2000;
+u64 send_interval = 3000;
 
 // Message data structure
 typedef struct message {
@@ -25,7 +27,7 @@ typedef struct message {
 } message;
 
 // Variable to hold data that is being sent
-message sendData;
+message messageData;
 
 /**
  * @brief Handles data send confirmation.
@@ -46,6 +48,8 @@ void on_data_sent(u8 *mac_addr, u8 status) {
  */
 void on_data_received(u8 *mac_addr, u8 *data, u8 length) {
   LOG_INFO("Message received");
+  memcpy(&messageData, data, length);
+  LOG_DEBUG(messageData.text);
 }
 
 void setup() {
@@ -63,33 +67,37 @@ void setup() {
     LOG_ERROR("Error initializing ESP-NOW");
     exit(1);
   }
-
-  esp_now_set_self_role(ESP_NOW_ROLE_MAX);
+  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
 
   LOG_DEBUG("Registering send/receive callbacks");
   esp_now_register_send_cb(on_data_sent);
   esp_now_register_recv_cb(on_data_received);
 
   LOG_DEBUG("Adding peers");
-  esp_now_add_peer(PEER_ADDRESSES[0], ESP_NOW_ROLE_MAX, 1, NULL, 0);
-  esp_now_add_peer(PEER_ADDRESSES[1], ESP_NOW_ROLE_MAX, 1, NULL, 0);
+  for (u8 peer_index = 0; peer_index < PEER_ADDRESS_LEN; peer_index++) {
+    esp_now_add_peer(PEER_ADDRESSES[peer_index], ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+  }
 
   LOG_INFO("Initialization complete");
 }
 
 void loop() {
   if ((millis() - last_time) > send_interval) {
+    last_time = millis();
+
     digitalWrite(LED_BUILTIN, HIGH);
 
     LOG_DEBUG("Preparing message");
-    strcpy(sendData.text, "Hello");
+    strcpy(messageData.text, "Hello");
 
-    for (u8 peer_index = 0; peer_index < 2; peer_index++) {
+    for (u8 peer_index = 0; peer_index < PEER_ADDRESS_LEN; peer_index++) {
       LOG_DEBUG("Sending message to peer");
-      esp_now_send(PEER_ADDRESSES[peer_index], (u8 *) &sendData, sizeof(sendData));
+      esp_now_send(PEER_ADDRESSES[peer_index], (u8 *) &messageData, sizeof(messageData));
+
+      LOG_DEBUG("Message sent");
     }
 
-    delay(500);
+    delay(1000);
   }
   digitalWrite(LED_BUILTIN, LOW);
 }
