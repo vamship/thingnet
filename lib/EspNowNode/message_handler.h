8463837fcac890ef656b8445c5ebbed88830cfeb
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2021-12-22 12:34:53 -0500

    Format file

--- lib/EspNowNode/message_handler.h
+++ lib/EspNowNode/message_handler.h
@@ -19,20 +19,20 @@ typedef struct PeerMessage
 enum ProcessingResult
 {
     /**
-   * @brief The message was successfully processed, and processing should be
-   * stopped.
-   */
+     * @brief The message was successfully processed, and processing should be
+     * stopped.
+     */
     handled,
 
     /**
-   * @brief The message was successfully processed, but can be processed by
-   * other processors as well.
-   */
+     * @brief The message was successfully processed, but can be processed by
+     * other processors as well.
+     */
     chain,
 
     /**
-   * @brief There was an error processing the message.
-   */
+     * @brief There was an error processing the message.
+     */
     error
 };
 
@@ -43,36 +43,36 @@ class MessageHandler
 {
 public:
     /**
-   * @brief Construct a new message handler object
-   */
+     * @brief Construct a new message handler object
+     */
     MessageHandler();
 
     /**
-   * @brief Returns a boolean value that determines whether or not the
-   * handler can/will handle the message.
-   *
-   * @param message A pointer to the message that the handler will
-   *        receive.
-   * @return true If the handler wants to handle the message.
-   * @return false If the handler does not want to handle the message.
-   */
+     * @brief Returns a boolean value that determines whether or not the
+     * handler can/will handle the message.
+     *
+     * @param message A pointer to the message that the handler will
+     *        receive.
+     * @return true If the handler wants to handle the message.
+     * @return false If the handler does not want to handle the message.
+     */
     bool can_handle(PeerMessage *message);
 
     /**
-   * @brief Processes a message and returns a result that reflects the
-   * result of the processing.
-   *
-   * @param message A pointer to the message that the handler will
-   *        receive.
-   * @return ProcessingResult::handled If the message was completely
-   *         handled by the processor and no further processing is
-   *         required.
-   * @return ProcessingResult::chain If the message was processed
-   *         successfully, but can be handled by other processors in the
-   *         chain.
-   * @return ProcessingResult::error If there was an error processing the
-   *         message.
-   */
+     * @brief Processes a message and returns a result that reflects the
+     * result of the processing.
+     *
+     * @param message A pointer to the message that the handler will
+     *        receive.
+     * @return ProcessingResult::handled If the message was completely
+     *         handled by the processor and no further processing is
+     *         required.
+     * @return ProcessingResult::chain If the message was processed
+     *         successfully, but can be handled by other processors in the
+     *         chain.
+     * @return ProcessingResult::error If there was an error processing the
+     *         message.
+     */
     ProcessingResult process(PeerMessage *message);
 };
 #endif
