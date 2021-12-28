86c212ec47c34b6fa8639723d87f85df3b6c7e93
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2021-12-28 18:06:42 -0500

    Streamline implementation

--- src/main.cpp
+++ src/main.cpp
@@ -14,23 +14,10 @@
 #include "esp_now_node.h"
 #include "server_node_manager.h"
 
-#include "message_handler.h"
-#include "peer_message_handler.h"
-#include "generic_message_handler.h"
-
-#define UPDATE_INTERVAL 6000
-#define UPDATE_NOTIFICATION_INTERVAL 2000
-#define PULSE_LONG_DURATION 1000
-#define PULSE_SHORT_DURATION 500
-
 // const u8 SERVER_MAC[] = {0x18, 0xfe, 0x34, 0xd4, 0x7e, 0x9a};
 const u8 SERVER_MAC[] = {0x1A, 0xFE, 0x34, 0xD4, 0x82, 0x2A};
 
 EspNowNode &node = EspNowNode::get_instance();
-
-Pulser *led;
-Timer *update_timer;
-Timer *led_reset_timer;
 
 void setup()
 {
@@ -40,46 +27,28 @@ void setup()
     ASSERT_OK(node.init());
 
     LOG_DEBUG("Determining run mode");
+
+    NodeManager *manager;
+
     if (node.has_mac_address((u8 *)SERVER_MAC))
     {
-        ASSERT_OK(node.set_node_manager(new ServerNodeManager(&node)));
-        LOG_INFO("Running as SERVER");
+        manager = new ServerNodeManager(&node);
+        LOG_INFO("Running in SERVER mode");
     }
     else
     {
         /// TODO: Need to implement this
-        LOG_INFO("Running as PEER")
+        manager = new ServerNodeManager(&node);
+        LOG_INFO("Running in CLIENT mode");
     }
 
-    LOG_DEBUG("Initializing LED");
-    led = new Pulser(LED_BUILTIN, PULSE_LONG_DURATION);
-    led->init();
-
-    LOG_DEBUG("Initialize timers");
-    update_timer = new Timer(UPDATE_INTERVAL);
-    led_reset_timer = new Timer(UPDATE_NOTIFICATION_INTERVAL);
-
-    update_timer->start();
+    ASSERT_OK(manager->init());
+    ASSERT_OK(node.set_node_manager(manager));
 
     LOG_INFO("Initialization complete");
 }
 
 void loop()
 {
-    if (update_timer->is_complete())
-    {
-        LOG_DEBUG("Running manager update");
-        node.update();
-
-        led->set_duration(PULSE_SHORT_DURATION);
-        led_reset_timer->start();
-    }
-
-    if (led_reset_timer->is_complete())
-    {
-        led->set_duration(PULSE_LONG_DURATION);
-        update_timer->start();
-    }
-
-    led->update();
+    node.update();
 }
