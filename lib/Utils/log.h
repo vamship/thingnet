9c51ce4f3d148cd246af229d26caa1cf9c697cb0
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2021-12-28 08:22:20 -0500

    Format files

--- lib/Utils/log.h
+++ lib/Utils/log.h
@@ -9,31 +9,32 @@
 #define LOG_LEVEL_DEBUG 3
 
 #ifdef LOG_ENABLED
-    #define LOG(level, message, ...) {\
-    Serial.print(level);\
-    Serial.printf(message __VA_OPT__(,) __VA_ARGS__);\
-    Serial.println();\
-}
+#define LOG(level, message, ...)                           \
+    {                                                      \
+        Serial.print(level);                               \
+        Serial.printf(message __VA_OPT__(, ) __VA_ARGS__); \
+        Serial.println();                                  \
+    }
 #else
-    #define LOG(level, message)
+#define LOG(level, message)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_ERROR
-    #define LOG_ERROR(message, ...) LOG("[ERROR] ", message __VA_OPT__(,) __VA_ARGS__)
+#define LOG_ERROR(message, ...) LOG("[ERROR] ", message __VA_OPT__(, ) __VA_ARGS__)
 #else
-    #define LOG_ERROR(...)
+#define LOG_ERROR(...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_INFO
-    #define LOG_INFO(message, ...) LOG("[INFO ] " , message __VA_OPT__(,) __VA_ARGS__)
+#define LOG_INFO(message, ...) LOG("[INFO ] ", message __VA_OPT__(, ) __VA_ARGS__)
 #else
-    #define LOG_INFO(...)
+#define LOG_INFO(...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_DEBUG
-    #define LOG_DEBUG(message, ...) LOG("[DEBUG] ", message __VA_OPT__(,) __VA_ARGS__)
+#define LOG_DEBUG(message, ...) LOG("[DEBUG] ", message __VA_OPT__(, ) __VA_ARGS__)
 #else
-    #define LOG_DEBUG(...)
+#define LOG_DEBUG(...)
 #endif
 
 #endif
