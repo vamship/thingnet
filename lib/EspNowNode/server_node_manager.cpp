e1dc2c1747d2ec601ad5fec3aa484f5d01c4c48f
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2022-01-07 17:29:45 -0500

    Increase advertisement duration

--- lib/EspNowNode/server_node_manager.cpp
+++ lib/EspNowNode/server_node_manager.cpp
@@ -12,7 +12,7 @@
 
 namespace thingnet
 {
-    const int __SERVER_NODE_ADVERTISE_DURATION = 5000;
+    const int __SERVER_NODE_ADVERTISE_DURATION = 30000;
     const u8 __BROADCAST_PEER[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
     ServerNodeManager::ServerNodeManager(EspNowNode *node) : NodeManager(node)
