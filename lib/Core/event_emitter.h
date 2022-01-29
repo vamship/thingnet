091457e28a4cfda2d9ac5215c7096e9532d76d7a
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2022-01-29 18:40:56 -0500

    Move template class implementation into header

--- lib/Core/event_emitter.h
+++ lib/Core/event_emitter.h
@@ -2,10 +2,15 @@
 #define __EVENT_EMITTER_H
 
 #include <Arduino.h>
+#include "log.h"
+#include "error_codes.h"
+
+using namespace thingnet::utils;
 
 namespace thingnet
 {
     const int __MAX_LISTENER_COUNT = 32;
+    static Logger *_event_emitter_logger = new Logger("event-emit");
 
     /**
      * @brief A generic event base class that can accept and manage a collection
@@ -94,6 +99,99 @@ namespace thingnet
          */
         void emit(T event_data);
     };
+
+    template <typename T>
+    Event<T>::Event()
+    {
+        this->listener_count = 0;
+    }
+
+    template <typename T>
+    Event<T>::~Event()
+    {
+        // Nothing to do here.
+    }
+
+    template <typename T>
+    int Event<T>::add_listener(void (*listener)(int event_type, T event_data))
+    {
+        LOG_TRACE(_event_emitter_logger, "Registering event listener");
+        for (int index = 0; index < this->listener_count; index++)
+        {
+            if (this->listeners[index] == listener)
+            {
+                LOG_WARN(_event_emitter_logger, "Listener has already been registered");
+                return RESULT_DUPLICATE;
+            }
+        }
+        this->listeners[this->listener_count] = listener;
+        this->listener_count++;
+        LOG_DEBUG(_event_emitter_logger, "Listener registered. Total listeners [%d]",
+                  this->listener_count);
+        return RESULT_OK;
+    }
+
+    template <typename T>
+    int Event<T>::remove_listener(void (*listener)(int event_type, T event_data))
+    {
+        LOG_TRACE(_event_emitter_logger, "Removing event listener");
+        u16 listener_index = this->listener_count;
+        for (u16 index = 0; index < this->listener_count; index++)
+        {
+            if (this->listeners[index] == listener)
+            {
+                listener_index = index;
+                break;
+            }
+        }
+
+        if (listener_index == this->listener_count)
+        {
+            LOG_WARN(_event_emitter_logger, "Cannot remove unregistered listener");
+            return RESULT_NO_EXIST;
+        }
+
+        LOG_TRACE(_event_emitter_logger, "Found listener at [%d]", listener_index);
+        for (u16 index = listener_index;
+             listener_index < this->listener_count - 1;
+             listener_index++)
+        {
+            this->listeners[index] = this->listeners[index + 1];
+        }
+
+        this->listener_count--;
+        this->listeners[this->listener_count] = 0;
+        LOG_DEBUG(_event_emitter_logger, "Listener removed. Total listeners [%d]",
+                  this->listener_count);
+
+        return RESULT_OK;
+    }
+
+    template <typename T>
+    EventEmitter<T>::EventEmitter(int event_type) : Event<T>()
+    {
+        this->event_type = event_type;
+    }
+
+    template <typename T>
+    EventEmitter<T>::~EventEmitter()
+    {
+        // Nothing to do here.
+    }
+
+    template <typename T>
+    void EventEmitter<T>::emit(T event_data)
+    {
+        LOG_DEBUG(_event_emitter_logger, "Emitting event [%d]. Listener count [%d]",
+                  event_data, this->listener_count);
+        for (u16 index = 0; index < this->listener_count; index++)
+        {
+            LOG_TRACE(_event_emitter_logger, "Notifying listener [%d]");
+            this->listeners[index](event_data);
+        }
+
+        LOG_TRACE(_event_emitter_logger, "Notified [%d] listeners", this->listener_count);
+    }
 }
 
 #endif
